b8029f3efff0f1c531c83e63c4b32a360961faa6
Johannes Richter <johannes.richter@hmmh.de>
2021-12-06 16:50:14 +0100

    switch/case szenario für routing

--- root/content/main-components/content.component.php
+++ root/content/main-components/content.component.php
@@ -5,6 +5,29 @@
 // PHP-Content
 echo "Seiten-Inhalt";
 
+    // Aktuelle Seite: $path = $_SERVER['REQUEST_URI'];
+    // switch/case für alle seiten zum anzeigen des contents:
+
+/**
+    switch ($path) {
+    case 'umfrage-startseite':
+        require('../page-components/umfrage-startseite.page.php');
+        break;
+    case 'umfrage-details':
+        require('../page-components/umfrage-details.page.php');
+        break;
+    case 'umfrage-ergebnisse':
+        require('../page-components/umfrage-ergebnisse.page.php');
+        break;
+    ...
+    default:
+        require(../page-components/home.page.php);
+    }
+ **/
+
+    // Alternativ, wenn euch das zu kompliziert erscheint, einfach header.component & footer.component auf jeder page
+    // einbinden und die einzelnen pages auf der home-page verlinken.
+
 ?>
 
 <!-- Hier kommt HTML-Content -->
